1d042bf9bab4b0e81e2a7853cf2399d2099f2329
lorenzo <lor.abbadessa@gmail.com>
2023-11-02 17:58:00 +0100

    non completa esercizio

---
+++ js.js
@@ -0,0 +1,11 @@
+const immagini = document.getElementById (carosello);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(img_foto);
+}
+
+for (let index = 0; index < immagini.length; index++) {
+    const sequenza = immagini[i];
+    
+}
