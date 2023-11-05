f07b011bd8b77fe1876cca6f4fd46126f0d56370
lorenzo <lor.abbadessa@gmail.com>
2023-11-05 16:56:55 +0100

    completa esercizio

--- js.js
+++ js.js
@@ -1,11 +1,53 @@
-const immagini = document.getElementById (carosello);
+let percorso = [
+    "consegna/img/01.webp",
+    "consegna/img/02.webp",
+    "consegna/img/03.webp",
+    "consegna/img/04.webp",
+    "consegna/img/05.webp"
+]
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log(img_foto);
+for (let i = 0; i < percorso.length; i++) {
+
+	immagine = `<img class="img" src="${percorso[i]}"></img>`;
+	document.getElementById("container").innerHTML += immagine;
 }
 
-for (let index = 0; index < immagini.length; index++) {
-    const sequenza = immagini[i];
+
+
+const immagini = document.getElementsByClassName("img");
+let imgPrima = 0;
+const imgUltima = percorso.length - 1;
+immagini[imgPrima].classList.add("visibile");
+
+
+
+
+document.getElementById("btnDx").addEventListener("click", function () {
+
+	if (imgPrima < imgUltima) {
+		//rimuovo la visibilità sull'immagine attuale
+		immagini[imgPrima].classList.remove("visibile");
+
+		//aggiorno il contatore dell'immagine attuale
+		imgPrima++;
+
+		//aggiungo la visibilità sulla prossima immagine
+		immagini[imgPrima].classList.add("visibile");
+	
+    }
     
-}
+
+});
+
+document.getElementById("btnSx").addEventListener("click", function () {
+	if (imgPrima > 0) {
+		//rimuovo la visibilità sull'immagine attuale
+		immagini[imgPrima].classList.remove("visibile");
+
+		//aggiorno il contatore dell'immagine attuale
+		imgPrima--;
+
+		//aggiungo la visibilità sulla prossima immagine
+		immagini[imgPrima].classList.add("visibile");
+	}
+});
